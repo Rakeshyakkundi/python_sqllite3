79a5d63fe4999b566f4cb8b1e0d60c990bf51697
Rakesh <rakeshyakkundi2k@gmail.com>
2020-07-11 23:29:38 +0530

    2

--- sql_con.py
+++ sql_con.py
@@ -4,17 +4,17 @@ conn = sqlite3.connect('python_pro.db')
 
 i=0
 while i != 999:
-    num = int(input("\nEnter : 1]Add Student\t2]Delete Student\t 3]Search student\t 4]Print all Students\t 5]Exit\n"))
+    num = int(input("\nEnter : 1]Add Student\t2]Delete Student\t 3]Search student\t 4]Print all Students details \t 5]Exit\n"))
     if num ==1:
         name = input("Enter student name :")
-        USN = int(input('Enter USN :'))
-        m1 = int(input("Marks one :"))
-        m2 = int(input("Enter Marks two :"))
-        m3 = int(input("Enter marks three :"))
+        USN = int(input('Enter your USN :'))
+        m1 = int(input("Marks Subject one marks :"))
+        m2 = int(input("Enter Subject two marks:"))
+        m3 = int(input("Enter Subject three marks :"))
         conn.execute(f"INSERT INTO Students  \
             VALUES ('{name}',{USN}, {m1}, {m2}, {m3} )")
         conn.commit()
-        print('Data Base is :\n')
+        print('Data in Data Base:\n')
         cursor = conn.execute(f"SELECT name,USN,m1,m2,m3 from Students")
         for data in cursor:
             for j in data:
@@ -24,6 +24,7 @@ while i != 999:
         conn.execute(f'DELETE from Students where USN={USN}')
         conn.commit()
         cursor = conn.execute(f"SELECT name,USN,m1,m2,m3 from Students")
+
         for data in cursor:
             for j in data:
                 print(j)
@@ -40,7 +41,7 @@ while i != 999:
             kit.sort(reverse = False)
             kit.pop(0)
             print("Average Score of two number is :",(kit[0]+kit[1])/2)
-            break
+        
         print('Search student not found')
     elif num==5:
         print('Bye')
